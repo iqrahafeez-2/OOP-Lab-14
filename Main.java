99c6a2f22d4ff29f6ddc7731d161ea9874784e2c
iqrahafeez-2 <146338521+iqrahafeez-2@users.noreply.github.com>
2024-05-22 20:14:09 +0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+package com.example.ooplab14;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/ooplab14/login.fxml"));
+        Scene scene = new Scene(loader.load());
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
